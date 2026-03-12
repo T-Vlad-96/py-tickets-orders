4696e74b270ecce3a2b2297595df6ec1ef89b4cc
user <user@MacBook-Air-user.local>
2026-03-12 17:56:24 +0200

    fix: remove unvalid MAX_PAGE_SIZE parameter from REST_FRAMEWORK dict in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
-    "MAX_PAGE_SIZE": 12
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
 }
